8114932711e7485d30f2f84df0bea423f27cabb2
Antoine.b <antoine.blondin01@gmail.com>
2015-11-17 13:26:18 +0100

    mise en place de la boucle du main

--- src/city/City.java
+++ src/city/City.java
@@ -3,7 +3,9 @@ package city;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
+import java.util.UUID;
 
 import content.*;
 import letter.*;
@@ -89,21 +91,45 @@ public class City
 		return this.postBox.add(letter);	
 	}
 	
+	public int getDays(){
+		return this.days;
+	}
+	
+	public void setDays(int days){
+		this.days=days;
+	}
+	
+	public Inhabitant getInhabitant(int id){
+		return this.inhabs.get(id);
+	}
+	
 	public static void main(String[] args){
-		BankAccount senderBankAccount = new BankAccount(411.);
-		BankAccount receiverBankAccount = new BankAccount(411.);
-		Inhabitant sender = new Inhabitant("inhab 1", senderBankAccount);
-		Inhabitant receiver = new Inhabitant("inhab 2", receiverBankAccount);
-		Content text = new Text("coucou");
-		Content money = new Money(20.);
-		Letter<Text> letter= new SimpleLetter(sender, receiver,text);
-		Letter<Money> letter1= new PromissoryNote(sender, receiver,money);
 		City city = new City();
-		city.addInhabitant(sender);
-		city.addInhabitant(receiver);
-		city.sendLetter(letter);
-		city.sendLetter(letter1);
-		city.distributeLetters();
+		city.setDays(7);
+		Random rand = new Random();
+		for(int i=0;i<100;i++){
+			city.addInhabitant(new Inhabitant("inhab" + i, new BankAccount(rand.nextInt(1000)))); 
+		}
+		for(int i=1;i<city.getDays();i++){
+			System.out.println("***********************************");
+			System.out.println("Day " + i);
+			city.distributeLetters();
+			int numberOfLetter = rand.nextInt(5)+1;
+			for(int j=0;j<numberOfLetter;j++){
+				int sender;
+				int receiver;
+				int type;
+				sender = rand.nextInt(100);
+				receiver = rand.nextInt(100);
+				type = rand.nextInt(2);
+				switch(type){
+				case 0 :city.sendLetter(new SimpleLetter(city.getInhabitant(sender), city.getInhabitant(receiver), new Text(UUID.randomUUID().toString())));
+						break;
+				case 1 :city.sendLetter(new PromissoryNote(city.getInhabitant(sender), city.getInhabitant(receiver), new Money(rand.nextInt(100))));
+						break;
+				}
+			}
+		}
 	}
 }
 
